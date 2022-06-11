380eed8d4e8447eee214dd61849a6d8827732c73
Bullseye14 <36154277+Bullseye14@users.noreply.github.com>
2022-06-11 15:33:39 +0200

    ball respawn on floor workingggg

--- TFG 22/Assets/Scripts/Minigame1/Balls/BallRespawn.cs
+++ TFG 22/Assets/Scripts/Minigame1/Balls/BallRespawn.cs
@@ -9,9 +9,24 @@ public class BallRespawn : MonoBehaviour
 
     private Vector3 intertia_;
 
+    private float timerOnFloor = 0f;
+    private bool touchingFloor = false;
+
     private void Start()
     {
         respawnPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if(touchingFloor)
+        {
+            if (timerOnFloor >= 5f)
+                RespawnBall();
+
+            else if (timerOnFloor < 5f)
+                timerOnFloor += Time.deltaTime;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -19,16 +34,29 @@ public class BallRespawn : MonoBehaviour
         // Respawning ball to origin when touching a respawn object
         if(collision.gameObject.tag == "Respawn")
         {
-            this.gameObject.transform.position = respawnPosition;
-
-            this.gameObject.transform.rotation = respawnRotation;
-
-            intertia_.x = intertia_.y = intertia_.z = 0.002f;
-
-            // Setting velocity to 0 when respawn
-            this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
-            this.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-            this.gameObject.GetComponent<Rigidbody>().inertiaTensor = intertia_;
+            RespawnBall();
         }
+
+        if(collision.gameObject.tag == "Floor")
+        {
+            touchingFloor = true;
+        }
+    }
+
+    private void RespawnBall()
+    {
+        this.gameObject.transform.position = respawnPosition;
+
+        this.gameObject.transform.rotation = respawnRotation;
+
+        intertia_.x = intertia_.y = intertia_.z = 0.002f;
+
+        // Setting velocity to 0 when respawn
+        this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        this.gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        this.gameObject.GetComponent<Rigidbody>().inertiaTensor = intertia_;
+
+        touchingFloor = false;
+        timerOnFloor = 0f;
     }
 }
